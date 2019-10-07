6dad43bd5d00456bdfd92f7a5f43565b0e762b8b
Alexander Harvey <harvey.alex+github@gmail.com>
2019-10-07 16:43:22 +1100

    change databaseurl variable

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     authRoutes       = require("./routes/index");    
-//mongoose.connect("mongodb://localhost/yelp_camp_v4", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://heroku1:heroku1@cluster0-p4bfn.mongodb.net/Cluster0?retryWrites=true&w=majority", {
+
+console.log(process.env.DATABASEURL)
+mongoose.connect("mongodb://localhost/yelp_camp_v4", {useNewUrlParser: true});
+/* mongoose.connect("mongodb+srv://heroku1:heroku1@cluster0-p4bfn.mongodb.net/Cluster0?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useCreateIndex:true
 }).then(() => {
@@ -24,8 +26,8 @@ mongoose.connect("mongodb+srv://heroku1:heroku1@cluster0-p4bfn.mongodb.net/Clust
 }
 ).catch(err => {
     console.log("Error", err.message);
-})
-app.use(bodyParser.urlencoded({extended: true}));
+});
+ */app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
